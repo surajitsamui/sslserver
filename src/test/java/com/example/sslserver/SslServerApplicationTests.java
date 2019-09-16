bab9d58473a4b8687d6a994429e988f9514cf03c
Surajit Samui <surajitsamui011@gmail.com>
2019-09-16 23:05:33 +0530

    First commit

---
+++ src/test/java/com/example/sslserver/SslServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.sslserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SslServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+            System.out.println("hi surajit samui");
+	}
+
+}
